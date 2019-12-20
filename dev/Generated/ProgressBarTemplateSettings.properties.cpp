630e08d92267103e731ec5662e78f16ae9765754
Karen Lai <biilai@microsoft.com>
2019-12-20 15:51:08 -0800

    indeterminateError and indeterminatePaused animation

--- dev/Generated/ProgressBarTemplateSettings.properties.cpp
+++ dev/Generated/ProgressBarTemplateSettings.properties.cpp
@@ -11,6 +11,7 @@ CppWinRTActivatableClassWithDPFactory(ProgressBarTemplateSettings)
 GlobalDependencyProperty ProgressBarTemplateSettingsProperties::s_ClipRectProperty{ nullptr };
 GlobalDependencyProperty ProgressBarTemplateSettingsProperties::s_ContainerAnimationEndPositionProperty{ nullptr };
 GlobalDependencyProperty ProgressBarTemplateSettingsProperties::s_ContainerAnimationEndPosition2Property{ nullptr };
+GlobalDependencyProperty ProgressBarTemplateSettingsProperties::s_ContainerAnimationMidPositionProperty{ nullptr };
 GlobalDependencyProperty ProgressBarTemplateSettingsProperties::s_ContainerAnimationStartPositionProperty{ nullptr };
 GlobalDependencyProperty ProgressBarTemplateSettingsProperties::s_ContainerAnimationStartPosition2Property{ nullptr };
 GlobalDependencyProperty ProgressBarTemplateSettingsProperties::s_IndicatorLengthDeltaProperty{ nullptr };
@@ -49,6 +50,17 @@ void ProgressBarTemplateSettingsProperties::EnsureProperties()
         s_ContainerAnimationEndPosition2Property =
             InitializeDependencyProperty(
                 L"ContainerAnimationEndPosition2",
+                winrt::name_of<double>(),
+                winrt::name_of<winrt::ProgressBarTemplateSettings>(),
+                false /* isAttached */,
+                ValueHelper<double>::BoxedDefaultValue(),
+                nullptr);
+    }
+    if (!s_ContainerAnimationMidPositionProperty)
+    {
+        s_ContainerAnimationMidPositionProperty =
+            InitializeDependencyProperty(
+                L"ContainerAnimationMidPosition",
                 winrt::name_of<double>(),
                 winrt::name_of<winrt::ProgressBarTemplateSettings>(),
                 false /* isAttached */,
@@ -95,6 +107,7 @@ void ProgressBarTemplateSettingsProperties::ClearProperties()
     s_ClipRectProperty = nullptr;
     s_ContainerAnimationEndPositionProperty = nullptr;
     s_ContainerAnimationEndPosition2Property = nullptr;
+    s_ContainerAnimationMidPositionProperty = nullptr;
     s_ContainerAnimationStartPositionProperty = nullptr;
     s_ContainerAnimationStartPosition2Property = nullptr;
     s_IndicatorLengthDeltaProperty = nullptr;
@@ -128,6 +141,16 @@ void ProgressBarTemplateSettingsProperties::ContainerAnimationEndPosition2(doubl
 double ProgressBarTemplateSettingsProperties::ContainerAnimationEndPosition2()
 {
     return ValueHelper<double>::CastOrUnbox(static_cast<ProgressBarTemplateSettings*>(this)->GetValue(s_ContainerAnimationEndPosition2Property));
+}
+
+void ProgressBarTemplateSettingsProperties::ContainerAnimationMidPosition(double value)
+{
+    static_cast<ProgressBarTemplateSettings*>(this)->SetValue(s_ContainerAnimationMidPositionProperty, ValueHelper<double>::BoxValueIfNecessary(value));
+}
+
+double ProgressBarTemplateSettingsProperties::ContainerAnimationMidPosition()
+{
+    return ValueHelper<double>::CastOrUnbox(static_cast<ProgressBarTemplateSettings*>(this)->GetValue(s_ContainerAnimationMidPositionProperty));
 }
 
 void ProgressBarTemplateSettingsProperties::ContainerAnimationStartPosition(double value)
